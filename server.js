caed8388f12c79041b9bbe784237ce2602fc945b
Sam Henderson <jipo_00@hotmail.com>
2020-08-21 16:30:52 +0100

    ready to deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -13,7 +14,14 @@ app.use(express.json({ extended: false }));
 app.use('/api/products', require('./routes/api/products'));
 app.use('/api/sendmail', require('./routes/api/sendmail'));
 
-app.get('/', (req, res) => res.send('API Running'));
+// Static assets for production
+if (process.env.NODE_ENV === 'PRODUCTION') {
+  // Set static
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 

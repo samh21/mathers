2087160d81bb5c91404ee0ba2bb777c401779001
Sam Henderson <jipo_00@hotmail.com>
2020-09-24 13:54:18 +0100

    fixed map

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -92,11 +92,10 @@ const Contact = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2137.1103523809684!2d-3.606811648100142!3d57.61260578099866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4885772ddad30c9f%3A0xbde3ddee518dbe19!2sMathers%20Ice%20cream!5e0!3m2!1sen!2suk!4v1600951556408!5m2!1sen!2suk"
             width="400"
             height="300"
-            frameborder="0"
-            style="border:0;"
-            allowfullscreen=""
-            aria-hidden="false"
-            tabindex="0"
+            frameBorder="0"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            tabIndex="0"
           ></iframe>
         </Box>
       </div>

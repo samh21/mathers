6478793c554826edd46d13f64faecfdb51a6f0e9
Sam Henderson <jipo_00@hotmail.com>
2020-09-25 13:45:07 +0100

    testing dotenv

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 const helmet = require('helmet');
-require('dotenv').config();
+// require('dotenv').config();
 
 const app = express();
 
